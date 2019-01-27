4d1a873518796dedef70600bbe936e7a232b75f4
Ed Page <eopage@gmail.com>
2019-01-26 22:27:54 -0700

    refactor(fixture): Split out child path

---
+++ src/fixture/child.rs
@@ -0,0 +1,87 @@
+use std::path;
+
+/// Access paths within [`TempDir`] for testing.
+///
+/// See [`ChildPath`] trait implementations.
+///
+/// ```rust
+/// use assert_fs::prelude::*;
+///
+/// let temp = assert_fs::TempDir::new().unwrap();
+/// let input_file = temp.child("foo.txt");
+/// input_file.touch().unwrap();
+/// temp.close().unwrap();
+/// ```
+///
+/// [`TempDir`]: struct.TempDir.html
+/// [`ChildPath`]: struct.ChildPath.html
+pub trait PathChild {
+    /// Access a path within the temp directory.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use assert_fs::prelude::*;
+    ///
+    /// let temp = assert_fs::TempDir::new().unwrap();
+    /// println!("{}", temp.path().display());
+    /// println!("{}", temp.child("foo/bar.txt").path().display());
+    /// temp.close().unwrap();
+    /// ```
+    fn child<P>(&self, path: P) -> ChildPath
+    where
+        P: AsRef<path::Path>;
+}
+
+impl PathChild for super::TempDir {
+    fn child<P>(&self, path: P) -> ChildPath
+    where
+        P: AsRef<path::Path>,
+    {
+        ChildPath::new(self.path().join(path.as_ref()))
+    }
+}
+
+/// A path within a [`TempDir`]
+///
+/// See Trait Implementations.
+///
+/// # Examples
+///
+/// ```rust
+/// use assert_fs::prelude::*;
+///
+/// let temp = assert_fs::TempDir::new().unwrap();
+///
+/// let input_file = temp.child("foo.txt");
+/// input_file.touch().unwrap();
+///
+/// temp.child("bar.txt").touch().unwrap();
+///
+/// temp.close().unwrap();
+/// ```
+///
+/// [`TempDir`]: struct.TempDir.html
+pub struct ChildPath {
+    path: path::PathBuf,
+}
+
+impl ChildPath {
+    /// Wrap a path for use with extension traits.
+    ///
+    /// See trait implementations or [`PathChild`] for more details.
+    ///
+    /// [`PathChild`]: trait.PathChild.html
+    pub fn new<P>(path: P) -> Self
+    where
+        P: Into<path::PathBuf>,
+    {
+        Self { path: path.into() }
+    }
+
+    /// Access the path.
+    pub fn path(&self) -> &path::Path {
+        &self.path
+    }
+}
+
